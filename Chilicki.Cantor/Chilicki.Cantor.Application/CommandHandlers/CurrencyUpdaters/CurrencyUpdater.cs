13de8c65ede7f9d63a85817773288473402c10f9
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-07-11 17:13:08 +0200

    Add console application for updating currencies

---
+++ Chilicki.Cantor/Chilicki.Cantor.Application/CommandHandlers/CurrencyUpdaters/CurrencyUpdater.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chilicki.Cantor.Application.CommandHandlers.CurrencyUpdaters
+{
+    public class CurrencyUpdater
+    {
+    }
+}
---
+++ Chilicki.Cantor/Chilicki.Cantor.CurrencyUpdater/Program.cs
@@ -0,0 +1,24 @@
+﻿using Chilicki.Cantor.Infrastructure.Repositories.Currencies;
+using Chilicki.Cantor.Infrastructure.Repositories.Currencies.Base;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Chilicki.Cantor.CurrencyUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<Application.CommandHandlers.CurrencyUpdaters.CurrencyUpdater>()
+                .AddTransient<ICurrencyRepository, CurrencyRepository>()
+                .BuildServiceProvider();
+
+
+            //do the actual work here
+            var currencyUpdater = serviceProvider.GetService<Application.CommandHandlers.CurrencyUpdaters.CurrencyUpdater>();
+            //currencyUpdater.DoSomeRealWork();
+        }
+    }
+}
