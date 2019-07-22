b9c6e82b60b32188e8d467281ef39bb62d0d84a5
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-07-22 18:45:50 +0200

    Add currencies/cantor endpoint

---
+++ Chilicki.Cantor/Chilicki.Cantor.WebAPI/Controllers/CurrenciesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chilicki.Cantor.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chilicki.Cantor.WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]    
+    public class CurrenciesController : ControllerBase
+    {
+        readonly IMediator _mediator;
+
+        public CurrenciesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("cantor")]
+        public async Task<IActionResult> GetCantorCurrencies([FromQuery]GetCantorCurrenciesQuery command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
